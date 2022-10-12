03ee994e32feadcb517bbc1f6631798d68865405
Renaldinho <rena0968@easv365.dk>
2022-10-12 15:31:04 +0200

    gitignore

---
+++ Frontend/src/services/http.service.ts
@@ -0,0 +1,13 @@
+import {Injectable, OnInit} from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
