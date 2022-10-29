57c051c5fb26156b0d0014ea79af38bf6fc06721
silamin <amine.aouina.lp.4@gmail.com>
2022-10-29 17:32:51 +0200

    form done

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -17,11 +17,20 @@ export class AppComponent implements OnInit{
   }
 
   async ngOnInit() {
+    let unit : string = (<HTMLInputElement>document.getElementById("myUnit")).value;
+
     //const boxes = await this.http.getProducts();
     //this.boxes=boxes;
   }
 
-  OpenForm() {
+  preFillForm() {
+    const nameInput = document.getElementById('box-name') as HTMLInputElement;
+    const boxDescriptionInput = document.getElementById('box-description') as HTMLInputElement;
+    const boxName = document.getElementById('name-input') as HTMLInputElement;
+    const boxDescription = document.getElementById('description-input') as HTMLInputElement;
+
+  boxName.value=nameInput.value;
+  boxDescription.value=boxDescriptionInput.value;
 
   }
 }
